75da6180e81da8175bdf36930f832f7b3fdbdbd8
R0onen <erni100105@gmail.com>
2024-04-21 09:49:24 +0500

    awdawd

--- backend.py
+++ backend.py
@@ -1,17 +1,7 @@
 from flask import Flask, render_template
-import database_conn as dbc
-import psycopg2
 
 app = Flask(__name__)
 
-def get_db_connection():
-    conn = psycopg2.connect(
-        host='localhost',
-        database='HackNU',
-        user='postgres',
-        password='admin'
-    )
-    return conn
 
 @app.route('/')
 def main():
